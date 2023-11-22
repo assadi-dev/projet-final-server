e95d03c06a8050bd5e64f3befef8500d2ecef9d4
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-11-22 20:19:58 +0100

    mise en place des relations table answers

---
+++ database/migrations/2023_11_22_191620_add_relations_answers_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class AddRelationsAnswersTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::table('answers', function (Blueprint $table) {
+            $table->foreignId("id_question")->constrained("questions");
+            $table->foreignId("id_participant")->constrained("participant");
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::table('answers', function (Blueprint $table) {
+            //
+        });
+    }
+}
