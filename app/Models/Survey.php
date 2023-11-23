086da66304f6cdf4cd92373e9754e3f70ea5f6aa
Marc AKPOTO-K <brightmarc90@gmail.com>
2023-11-23 15:42:32 +0100

    Création du modèle Question
    - Création du modèle
    - realtions entre les modèles questions et sondage

--- app/Models/Survey.php
+++ app/Models/Survey.php
@@ -4,6 +4,7 @@ namespace App\Models;
 
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
+use Illuminate\Database\Eloquent\Relations\HasMany;
 
 class Survey extends Model
 {
@@ -18,4 +19,13 @@ class Survey extends Model
         'title',
         'description'
     ];
+
+    /**
+     * questions
+     * Récupère les les questions liées à un sondage
+     * @return HasMany
+     */
+    public function questions(): HasMany {
+        return $this->hasMany(Question::class);
+    }
 }
