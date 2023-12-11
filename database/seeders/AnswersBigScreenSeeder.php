e48dbfd8916e2e15ed26d44ff89e255cea2cea61
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-11 21:37:11 +0100

    function generation des reponses aleatoire

--- database/seeders/AnswersBigScreenSeeder.php
+++ database/seeders/AnswersBigScreenSeeder.php
@@ -10,6 +10,13 @@ use Illuminate\Database\Seeder;
 
 class AnswersBigScreenSeeder extends Seeder
 {
+    private $faker;
+
+    public function __construct()
+    {
+        $this->faker = Factory::create();
+    }
+
     /**
      * Run the database seeds.
      *
@@ -18,44 +25,82 @@ class AnswersBigScreenSeeder extends Seeder
     public function run()
     {
         //$answersSaved = Participant::save_answers($email, $survey_id, $answers);
-        dd($this->bigScreenQuestion());
+        // dd($this->faker->email());
 
+        dd($this->generate_answers($this->faker->email(), null));
 
     }
 
 
     private function generate_participant()
     {
-        $faker = Factory::create();
 
-        $email = $faker->email();
+
+        $email = $this->faker->email();
         $survey_id = Survey::findByTitle("Sondage Big Screen")->id;
 
 
     }
 
-    private function generate_answers($email, $survey_id) {}
+    private function generate_answers($email, $survey_id)
+    {
+
+        $bigScreenQuestions = $this->bigScreenQuestion();
+
+        $cb = function ($questionItem) use ($email) {
+            $type = $questionItem["type"];
+            $propositions = $questionItem["propositions"];
+            if($questionItem["body"] == "votre adresse email") {
+                $value = $email;
+            } else {
+                $value = $this->generate_answer_by_type($type, $propositions);
+            }
+
+            return ["question_id" => $questionItem["question_id"],"value" => $value ];
+        };
+        return  array_map($cb, $bigScreenQuestions);
+    }
 
 
+    /**
+     * génération d'un tableau contenant les question de bigscreen
+     */
     private function bigScreenQuestion()
     {
         // chemin d'accès à votre fichier JSON
         $file = public_path("questions.json");
         // décoder le flux JSON
-        $questions = json_decode(file_get_contents($file));
+        $questions = json_decode(file_get_contents($file), true);
 
         $cb =  function ($v) {
-            $title =  $v->body;
-            $question = Question::findbyQuestionBody($title);
+            $body =  $v["body"];
+            $question = Question::findbyQuestionBody($body);
             $propositions = null;
-            if(isset($v->propositions)) {
-                $propositions = $v->propositions;
+            if(isset($v["propositions"])) {
+                $propositions = $v["propositions"];
             }
 
-            return ["question_id" =>  $question->id,"title" => $title,  "type" => $v->type,"propositions" => $propositions];
+            return ["question_id" =>  $question->id,"body" => $body,  "type" => $v["type"],"propositions" => $propositions];
         };
 
-        return array_map($cb, $questions);
+        return  array_map($cb, $questions);
+
+    }
+
+    private function generate_answer_by_type($type, $propositions)
+    {
+
+        switch ($type) {
+            case 'A':
+                return $this->faker->randomElement($propositions);
+            case 'B':
+                return $this->faker->words(5, true);
+                ;
+            case 'C':
+                return $this->faker->numberBetween(1, 5);
+
+
+        }
 
     }
 
