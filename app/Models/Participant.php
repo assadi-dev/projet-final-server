d2e56930a54ff44091ed4ec7de7f9452d56b8b8e
Assadi <62776480+assadi-dev@users.noreply.github.com>
2023-12-11 18:23:29 +0100

    refacto creation du fonction enregistrement des reponses

--- app/Models/Participant.php
+++ app/Models/Participant.php
@@ -38,4 +38,56 @@ class Participant extends Model
     {
         return $this->hasMany(Answer::class);
     }
+
+    /**
+     * Creation du participant + enregistrement de ses reponses
+     */
+    public static function save_answers($email, $survey_id, $answers)
+    {
+        try {
+
+            if(!isset($email)) {
+                throw new \Exception("email non renseigné");
+            }
+            if(!isset($survey_id)) {
+                throw new \Exception("survey_id non renseigné");
+            }
+            if(!isset($answers)) {
+                throw new \Exception("answers non renseigné");
+            }
+
+            $answerParticipant = [];
+            $participant = null;
+
+            //creation de l'entité participant
+            $participant = Participant::create(
+                [
+                   "email" => $email,
+                   "token" => base64_encode($email),
+                   "survey_id" => $survey_id
+                   ]
+            );
+
+            //Sauvegardes des reponses
+            foreach($answers as $answer) {
+                $answer = Answer::create([
+                    "value" => $answer["value"],
+                    "email" => $email,
+                    "survey_id" => $survey_id,
+                    "question_id" => $answer["question_id"],
+                ]);
+
+                array_push($answerParticipant, $answer);
+
+            };
+
+            return  (object)["participants" => $participant,"answers" => $answerParticipant];
+
+        } catch (\Throwable $th) {
+            throw $th;
+        }
+
+
+    }
+
 }
